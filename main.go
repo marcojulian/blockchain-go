18aeab710303b881f035246569b819fd1c277fa8
Marco Julián Torre <marco.julian.torre@gmail.com>
2022-11-09 11:30:12 -0300

    Implemented addBlock for BlockChain, and moved both structs to files for each of them

--- main.go
+++ main.go
@@ -1,43 +1,11 @@
 package main
 
 import (
-	"crypto/sha256"
-	"encoding/json"
-	"fmt"
-	"strings"
 	"time"
 )
 
-type Block struct {
-	data         map[string]any
-	hash         string
-	previousHash string
-	timestamp    time.Time
-	pow          int
-}
-
-type BlockChain struct {
-	genesisBlock Block
-	chain        []Block
-	difficulty   int
-}
-
 func main() {
 
-}
-
-func (b Block) calculateHash() string {
-	data, _ := json.Marshal(b.data)
-	blockData := b.previousHash + string(data) + b.timestamp.String()
-	blockHash := sha256.Sum256([]byte(blockData))
-	return fmt.Sprintf("%x", blockHash)
-}
-
-func (b *Block) mine(difficulty int) {
-	for !strings.HasPrefix(b.hash, strings.Repeat("0", difficulty)) {
-		b.pow++
-		b.hash = b.calculateHash()
-	}
 }
 
 func CreateBlockchain(difficulty int) BlockChain {

66bbc0dd69973bf63b02c263897451a1c2c4681c
Marco Julián Torre <marco.julian.torre@gmail.com>
2022-11-09 11:11:30 -0300

    Added block struct

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
+import "time"
+
+type Block struct {
+	data         map[string]any
+	hash         string
+	previousHash string
+	timestamp    time.Time
+	pow          int
+}
+
 func main() {
 
 }

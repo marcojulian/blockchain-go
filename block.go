104f9b4c87978c823390c4bb4f8c642928aa1903
Marco Julián Torre <marco.julian.torre@gmail.com>
2022-11-09 11:43:43 -0300

    Fix on calculateHash and POC in main method

--- block.go
+++ block.go
@@ -4,6 +4,7 @@ import (
 	"crypto/sha256"
 	"encoding/json"
 	"fmt"
+	"strconv"
 	"strings"
 	"time"
 )
@@ -18,7 +19,7 @@ type Block struct {
 
 func (b Block) calculateHash() string {
 	data, _ := json.Marshal(b.data)
-	blockData := b.previousHash + string(data) + b.timestamp.String()
+	blockData := b.previousHash + string(data) + b.timestamp.String() + strconv.Itoa(b.pow)
 	blockHash := sha256.Sum256([]byte(blockData))
 	return fmt.Sprintf("%x", blockHash)
 }
--- main.go
+++ main.go
@@ -1,11 +1,20 @@
 package main
 
 import (
+	"fmt"
 	"time"
 )
 
 func main() {
+	// create a new blockchain with a mining difficulty of 2
+	blockchain := CreateBlockchain(2)
 
+	// record transactions on the blockchain from Alice, Bob, and John
+	blockchain.addBlock("Alice", "Bob", 5)
+	blockchain.addBlock("John", "Bob", 2)
+
+	// check if the blockchain is valid; expecting true
+	fmt.Println(blockchain.isValid())
 }
 
 func CreateBlockchain(difficulty int) BlockChain {
